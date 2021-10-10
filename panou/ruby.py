3e12b0dc7bb06b751adae611bd2bec5a614e100c
AgapeIoan <ioanagape14@gmail.com>
2021-10-10 20:23:36 +0300

    Nici nush ce am facut aici
    Ceva migrare de la dislash la disnake parca, dar tot nu o iesit complet

--- panou/ruby.py
+++ panou/ruby.py
@@ -3,23 +3,39 @@ import requests
 
 from bs4 import BeautifulSoup
 from functii.samp import vezi_asociere
-from functii.creier import scrape_panou, get_nickname, login_panou, este_player_online
+from functii.creier import scrape_panou, get_nickname, login_panou, este_player_online, get_server_provenienta, headers
 
-def stats(player):
+def get_panel_data(player):
+    with requests.Session() as s:
+        
+        # TODO Ceva sa trag tot panel-ul si sa il salvez in memorie.
+        # O sa verific daca player-ul exista INAINTE de a afisa butoanele
+        
+        # O sa am panel intr-un format ce o sa fie prelucrat in parte de vstats, stats, etc, FARA CONEXIUNE LA PANEL? SAU
+        # la prima verificare a panel-ului sa imi salvez tot panel-ul undeva?
+        # Imo cred ca ar fi mai bine sa merg pe prima varianta, cuz daca tot verific existenta player-ului,
+        # automat o sa am tot panel-ul descarcat in memorie
+        url = f'http://rubypanel.nephrite.ro/profile/{player}'
+        r = s.get(url, headers=headers)
+        soup = BeautifulSoup(r.content, features='html5lib')
 
+        if not get_nickname(soup):
+            return None
+        
+        return soup
+
+
+
+
+def stats(soup):
     with requests.Session() as s:
         lista_valori_scrape = [
             {'div': {'class': 'col-md-8'}},
-            {'h3': {'class': 'profile-username'}},
             {'div': {'class': 'col-md-12 col-lg-12'}},
             {'div': {'class': 'alert bg-red'}}
         ]
 
-        url = f'http://rubypanel.nephrite.ro/profile/{player}'
-        psot, user_name_panou, badges_raw, ban_status_raw = scrape_panou(s, url, lista_valori_scrape)
-
-        if not psot:
-            return 'Nu am putut gasi jucatorul specificat. Verifica daca ai introdus corect nickname-ul!'
+        psot, badges_raw, ban_status_raw = scrape_panou(soup, lista_valori_scrape)
 
         try:
             ban_string = ban_status_raw[0].findAll('h4')
@@ -32,8 +48,12 @@ def stats(player):
         except IndexError:
             motiv_string_lista = []
 
-        nickname_player, player_online, server_de_provenienta = este_player_online(
-            str(user_name_panou), player)
+        # nickname_player, player_online, server_de_provenienta = este_player_online(
+        #     str(user_name_panou), player)
+
+        nickname_player = get_nickname(soup)
+        player_online = "online" if este_player_online(soup) else "offline"
+        server_de_provenienta = get_server_provenienta(soup)
 
         embed = discord.Embed(
             title=nickname_player, description="Status: " + player_online, color=0x00ff00)
@@ -78,18 +98,6 @@ def stats(player):
         embed.set_footer(text="Ruby Nephrite | ruby.nephrite.ro:7777")
         print("STATS DATA WAS RETURNED")
         return embed
-
-async def stats_debug(player):
-    embed = discord.Embed(
-    title="nickDEBUG", description="Status: " + "DEBUGGING", color=0x00ff00)
-    embed.set_thumbnail(url="https://i.imgur.com/mZvN9jZ.png")
-        
-
-
-
-async def vstats_debug(inter, player):
-    car_list = [['Sandking (ID:9803)', '8997 kilometers', 'No', '1534 days', 'display location'], ['Comet (ID:14739)', '435 kilometers', 'No', '1507 days', 'display location'], ['NRG-500 (ID:19621)', '24400 kilometers', 'No', '1475 days', 'display location'], ['Bike (ID:19825)', '4 kilometers', 'No', '1474 days', 'display location'], ['Sultan (ID:25697)  VIP text: LiderAdv', '8853 kilometers', 'Green', '1439 days', 'display location'], ['Alpha (ID:29007)', '24 kilometers', 'No', '1418 days', 'display location'], ['Phoenix (ID:75826)  VIP text: Agency 510', '26 kilometers', 'No', '1104 days', 'display location'], ['Romero (ID:80068)  VIP text: Alexandrescu', '143 kilometers', 'No', '1070 days', 'display location'], ['Fortune (ID:166619)   Formerly ID: 50996', '415 kilometers', 'No', '1000 days', 'display location'], ['Premier (ID:103088)  VIP text: Taxi', '161 kilometers', 'No', '963 days', 'display location'], ['Stretch (ID:108589)  VIP text: TesterAdv', '299 kilometers', 'No', '908 days', 'display location'], ['Huntley (ID:123183)', '0 kilometers', 'No', '837 days', 'display location'], ['Bloodring Banger (ID:123944)', '746 kilometers', 'No', '832 days', 'display location'], ['Roadtrain (ID:187391)', '1271 kilometers', 'No', '563 days', 'display location'], ['Picador (ID:212281)  VIP text: SILV Rank 2', '11 kilometers', 'No', '519 days', 'display location'], ['Vortex (ID:225291)', '41 kilometers', 'No', '444 days', 'display location'], ['Huntley (ID:235951)', '0 kilometers', 'No', '389 days', 'display location'], ['Huntley (ID:273219)', '0 kilometers', 'No', '211 days', 'display location'], ['Buffalo (ID:287536)', '0 kilometers', 'No', '126 days', 'display location'], ['FCR-900 (ID:290093)', '0 kilometers', 'No', '95 days', 'display location'], ['ZR-350 (ID:290577)  VIP text: NULL', '0 kilometers', 'No', '92 days', 'display location'], ['Phoenix (ID:290596)', '0 kilometers', 'No', '92 days', 'display location'], ['Infernus (ID:295078)', '0 kilometers', 'No', '67 days', 'display location']]
-    return car_list
 
 async def vstats(inter, player):
     with requests.Session() as s:
@@ -148,3 +156,14 @@ async def extract_cars(f2):
         for i in lista_de_trimis:
             print(i)
         return lista_de_trimis
+
+
+        
+async def stats_debug(player):
+    embed = discord.Embed(
+    title="nickDEBUG", description="Status: " + "DEBUGGING", color=0x00ff00)
+    embed.set_thumbnail(url="https://i.imgur.com/mZvN9jZ.png")
+
+async def vstats_debug(inter, player):
+    car_list = [['Stretch (ID:128170)   Formerly ID: 47132 VIP text: ksn', '8997 kilometers', 'White', '1534 days', 'display location'], ['Comet (ID:14739)', '435 kilometers', 'No', '1507 days', 'display location'], ['NRG-500 (ID:19621)', '24400 kilometers', 'No', '1475 days', 'display location'], ['Bike (ID:19825)', '4 kilometers', 'No', '1474 days', 'display location'], ['Sultan (ID:25697)  VIP text: LiderAdv', '8853 kilometers', 'Green', '1439 days', 'display location'], ['Alpha (ID:29007)', '24 kilometers', 'No', '1418 days', 'display location'], ['Phoenix (ID:75826)  VIP text: Agency 510', '26 kilometers', 'No', '1104 days', 'display location'], ['Romero (ID:80068)  VIP text: Alexandrescu', '143 kilometers', 'No', '1070 days', 'display location'], ['Fortune (ID:166619)   Formerly ID: 50996', '415 kilometers', 'No', '1000 days', 'display location'], ['Premier (ID:103088)  VIP text: Taxi', '161 kilometers', 'No', '963 days', 'display location'], ['Stretch (ID:108589)  VIP text: TesterAdv', '299 kilometers', 'No', '908 days', 'display location'], ['Huntley (ID:123183)', '0 kilometers', 'No', '837 days', 'display location'], ['Bloodring Banger (ID:123944)', '746 kilometers', 'No', '832 days', 'display location'], ['Roadtrain (ID:187391)', '1271 kilometers', 'No', '563 days', 'display location'], ['Picador (ID:212281)  VIP text: SILV Rank 2', '11 kilometers', 'No', '519 days', 'display location'], ['Vortex (ID:225291)', '41 kilometers', 'No', '444 days', 'display location'], ['Huntley (ID:235951)', '0 kilometers', 'No', '389 days', 'display location'], ['Huntley (ID:273219)', '0 kilometers', 'No', '211 days', 'display location'], ['Buffalo (ID:287536)', '0 kilometers', 'No', '126 days', 'display location'], ['FCR-900 (ID:290093)', '0 kilometers', 'No', '95 days', 'display location'], ['ZR-350 (ID:290577)  VIP text: NULL', '0 kilometers', 'No', '92 days', 'display location'], ['Phoenix (ID:290596)', '0 kilometers', 'No', '92 days', 'display location'], ['Infernus (ID:295078)', '0 kilometers', 'No', '67 days', 'display location']]
+    return car_list
