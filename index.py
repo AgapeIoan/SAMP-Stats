267d1e9add1f13b280a5a206e84bf97de19e335e
AgapeIoan <ioanagape14@gmail.com>
2021-10-16 21:22:27 +0300

    Adaugat culoare embed la status online/offline

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 import json
 import disnake
 import asyncio
-from disnake import embeds
-from disnake import player
 
+from disnake import ActionRow, Button, ButtonStyle, SelectMenu, SelectOption, Option, OptionType
+from disnake import emoji
 from disnake.ext import commands
-from dislash import InteractionClient, ActionRow, Button, ButtonStyle, SelectMenu, SelectOption, ContextMenuInteraction, Option, OptionType
 
 import panou.ruby
+import clase_menus
 
 with open("config.json", "r") as f:
     config = json.load(f)
@@ -16,186 +16,102 @@ test_guilds = [722442573137969174]
 BOT_TOKEN = config["BOT_TOKEN"]
 POZA_MASINA_SAMP = "https://i.imgur.com/KC9rlJd.png"
 
-bot = commands.Bot(command_prefix="!")
-inter_client = InteractionClient(bot, test_guilds = [722442573137969174])
+bot = commands.Bot(command_prefix="!", test_guilds = [722442573137969174])
 # If 'test_guilds' param isn't specified, the commands are registered globally.
 # Global registration takes up to 1 hour.
 
-def lista_butoane_stats(stats=False, vstats=False, bstats=False, fstats=False, cstats=False):
-    return ActionRow(
-        Button(
-            style=ButtonStyle.blurple,
-            label="Player Stats",
-            custom_id="stats_button",
-            disabled=stats
-        ),
-        Button(
-            style=ButtonStyle.blurple,
-            label="Vehicles",
-            custom_id="vehicles_button",
-            disabled=vstats
-        ),
-        Button(
-            style=ButtonStyle.blurple,
-            label="Properties",
-            custom_id="properties_button",
-            disabled=bstats
-        ),
-        Button(
-            style=ButtonStyle.blurple,
-            label="Faction History",
-            custom_id="faction_button",
-            disabled=fstats
-        ),
-        Button(
-            style=ButtonStyle.blurple,
-            label="Clan",
-            custom_id="clan_button",
-            disabled=cstats
-        )
-    )
+@bot.slash_command()
+async def ping(inter):
+    await inter.response.send_message('pong')
 
-def create_car_embed(car_stats, nickname):
-    embed=discord.Embed(color=0x00ff00)
-
-    formated_car_stats = ''
-
-    # 'Stretch (ID:128170)   Formerly ID: 47132 VIP text: ksn'
-    if "VIP text:" in car_stats[0]:
-        # De exemplu, din "Picador (ID:212281)  VIP text: SILV Rank 2"
-        # o sa extragem doar "VIP text: SILV Rank 2"
-        vip_text = car_stats[0][car_stats[0].find("VIP text: "):]
-        formated_car_stats+=vip_text+'\n'
-        car_stats[0] = car_stats[0].replace(vip_text, '').strip()
-
-    if "Formerly ID: " in car_stats[0]:
-        # 'Fortune (ID:166619)   Formerly ID: 50996'
-        # Nu are cum sa faca cineva prank cu Formerly ID in VIP text, ca VIP text-ul se scoate in if-ul de mai sus :creier:
-        formerly_id = car_stats[0][car_stats[0].find("Formerly ID: "):]
-        formated_car_stats+=formerly_id+'\n'
-        car_stats[0] = car_stats[0].replace(formerly_id, '').strip()
-
-    formated_car_stats+=f"{car_stats[1]}\n{car_stats[3]}\n"
-    if car_stats[2] != "No":
-        # Avem neon
-        formated_car_stats+=f"Neon: {car_stats[2]}"
-
-    embed.set_thumbnail(url="https://i.imgur.com/KC9rlJd.png")
-    embed.add_field(name=car_stats[0], value=formated_car_stats, inline=False)
-
-    embed.set_footer(text=f"{nickname} | ruby.nephrite.ro")
-
-    return embed
-
-@bot.listen()
-async def on_ready():
-    print("Hatz cu buna dimineata, a pornit botu")
-
-@inter_client.user_command(name="Press me")
-async def press_me(inter):
-    # User commands are visible in user context menus
-    # They can be global or per guild, just like slash commands
-    await inter.respond("Hello there!")
-
-@inter_client.message_command(name="Resend")
-async def resend(inter):
-    # Message commands are visible in message context menus
-    # inter is instance of ContextMenuInteraction
-    await inter.respond(inter.message.content)
-
-
-@inter_client.slash_command(
+@bot.slash_command(
     name="buton", # Defaults to the function name
     description="butoane",
     guild_ids=test_guilds,
     options=[
-        Option("nickname", "Introdu nickname-ul", OptionType.STRING, required=True)
+        Option("nickname", "Introdu nickname-ul", OptionType.string, required=True)
         # By default, Option is optional
         # Pass required=True to make it a required arg
     ]
 )
-async def buton(ctx, nickname):
-    msg = await ctx.send(content="test", type=5)
-    await asyncio.sleep(3.2)
+async def buton(inter, nickname):
+    await inter.response.defer()
 
     try:
         soup = panou.ruby.get_panel_data(nickname)
     except IndexError:
         # TODO Sa isi dea seama bot-ul daca e vorba de panel picat, pagina blank, lipsa profil sau orice altceva se poate intampla
         # si sa afiseze un mesaj de eroare corespunzator
-        await ctx.send(f"Jucatorul **{nickname}** nu a fost gasit. Verifica daca ai introdus corect nickname-ul!", ephemeral=True)
+        await inter.edit_original_message(f"Jucatorul **{nickname}** nu a fost gasit. Verifica daca ai introdus corect nickname-ul!", ephemeral=True)
         return
 
-    await ctx.edit_original_message("Test")
-    # msg = await msg.edit(content="**Selecteaza o optiune:**", components=[lista_butoane_stats()], type=4)
-    on_click = msg.create_click_listener(timeout=60) # TODO Vedem ce iese si marim timeout la nevoie
+    view = clase_menus.Main_Menu(soup)
 
-    @on_click.not_from_user(ctx.author, cancel_others=True, reset_timeout=False)
-    async def on_wrong_user(inter):
-        await inter.reply("You're not the author", ephemeral=True)
+    await inter.edit_original_message(content="**Selecteaza o optiune:**", view=view)
 
-    @on_click.matching_id("stats_button", reset_timeout=True)
-    async def on_test_button(inter):
-        #await inter.reply(content='**Procesez comanda...**', components=[], embed=None, type=7)
+    # @on_click.not_from_user(inter.author, cancel_others=True, reset_timeout=False)
+    # async def on_wrong_user(inter):
+    #     await inter.reply("You're not the author", ephemeral=True)
 
-        await msg.edit(content=None, embed=panou.ruby.stats(soup), components=[lista_butoane_stats(stats=True)])
-        # TODO In momentul in care aflam datele pe care vrem sa le afisam, salvam embed-ul undeva local pe aici si il folosim iar
-        # daca respectivul vrea sa revina la meniul anterior, in caz ca pleaca la alt meniu 
+    # @on_click.matching_id("vehicles_button", reset_timeout=True)
+    # async def on_test_button(inter):
+    #     await inter.reply(content='**Procesez comanda...**', components=[], embed=None, type=7)
 
-    @on_click.matching_id("vehicles_button", reset_timeout=True)
-    async def on_test_button(inter):
-        await inter.reply(content='**Procesez comanda...**', components=[], embed=None, type=7)
+    #     aux=[]
+    #     lista_masini = await panou.ruby.vstats_debug(inter, nickname)
+    #     # TODO Lista masini sa fie splited in grupe de cate 23. Prima optiune BACK te duce inapoi, ultima optiune NEXT te duce la urmatoarea grupa.
+    #     # BACK-ul cand esti la prima lista te duce la meniul principal aka lista butoane panou
+    #     aux.append(SelectOption("🔙 Inapoi 🔙", "back"))
+    #     for masina in lista_masini:
+    #         stats_masina_packed = f"{masina[0]}+{masina[1]}+{masina[2]}+{masina[3]}"
+    #         aux.append(SelectOption(masina[0], stats_masina_packed))
+    #         print(masina)
+    #     aux.append(SelectOption("➡️ Inainte ➡️", "next"))
 
-        aux=[]
-        lista_masini = await panou.ruby.vstats_debug(inter, nickname)
-        # TODO Lista masini sa fie splited in grupe de cate 23. Prima optiune BACK te duce inapoi, ultima optiune NEXT te duce la urmatoarea grupa.
-        # BACK-ul cand esti la prima lista te duce la meniul principal aka lista butoane panou
-        aux.append(SelectOption("🔙 Inapoi 🔙", "back"))
-        for masina in lista_masini:
-            stats_masina_packed = f"{masina[0]}+{masina[1]}+{masina[2]}+{masina[3]}"
-            aux.append(SelectOption(masina[0], stats_masina_packed))
-            print(masina)
-        aux.append(SelectOption("➡️ Inainte ➡️", "next"))
+    #     row_cars = ActionRow(
+    #         SelectMenu(
+    #             custom_id="test",
+    #             placeholder="Selecteaza o masina",
+    #             max_values=1,
+    #             options=aux
+    #         )
+    # )
+    #     # row_cars.components[0].options[INDEX_MASINA].default = True
+    #     await msg.edit(content="**Selecteaza o masina:**", components=[row_cars])
 
-        row_cars = ActionRow(
-            SelectMenu(
-                custom_id="test",
-                placeholder="Selecteaza o masina",
-                max_values=1,
-                options=aux
-            )
-    )
-        # row_cars.components[0].options[INDEX_MASINA].default = True
-        await msg.edit(content="**Selecteaza o masina:**", components=[row_cars])
-
-        values = [None]
-        while values[0] != "back":
-            inter = await msg.wait_for_dropdown()
-            print(inter)
-            # Send what you received
-            # labels = [option.label for option in inter.select_menu.selected_options]
-            values = [option.value for option in inter.select_menu.selected_options]
-            print(values)
+    #     values = [None]
+    #     while values[0] != "back":
+    #         inter = await msg.wait_for_dropdown()
+    #         print(inter)
+    #         # Send what you received
+    #         # labels = [option.label for option in inter.select_menu.selected_options]
+    #         values = [option.value for option in inter.select_menu.selected_options]
+    #         print(values)
             
-            if values[0] == "back":
-                # Ne intoarcem la meniul anterior cu butoane selectie stats
-                await inter.reply(content="**Selecteaza o optiune:**", embed=None, components=[row], type=7)
-            else:
-                # Trimitem frumos embed cu masina ceruta
-                # Embedul o sa fie generat pe baza dev-value a selectiei
-                await inter.reply(content='', embed=create_car_embed(values[0].split("+"), nickname), components=[row_cars], type=7)
+    #         if values[0] == "back":
+    #             # Ne intoarcem la meniul anterior cu butoane selectie stats
+    #             await inter.reply(content="**Selecteaza o optiune:**", embed=None, components=[row], type=7)
+    #         else:
+    #             # Trimitem frumos embed cu masina ceruta
+    #             # Embedul o sa fie generat pe baza dev-value a selectiei
+    #             await inter.reply(content='', embed=create_car_embed(values[0].split("+"), nickname), components=[row_cars], type=7)
 
-    @on_click.timeout
-    async def on_timeout():
-        await msg.edit(content="Mesajul a fost inactiv pentru prea mult timp, astfel butoanele au fost dezactivate.", components=[lista_butoane_stats(True, True, True, True, True)])
-        await asyncio.sleep(180)
-        # TODO Ar fi bine sa verificam inainte de sleep daca picam in except-ul de mai jos, astfel economisim timp si resurse yeeee
-        try:
-            await msg.edit(content='', components=[])
-        except discord.errors.HTTPException:
-            pass
+    # @on_click.timeout
+    # async def on_timeout():
+    #     await msg.edit(content="Mesajul a fost inactiv pentru prea mult timp, astfel butoanele au fost dezactivate.", components=[lista_butoane_stats(True, True, True, True, True)])
+    #     await asyncio.sleep(180)
+    #     # TODO Ar fi bine sa verificam inainte de sleep daca picam in except-ul de mai jos, astfel economisim timp si resurse yeeee
+    #     try:
+    #         await msg.edit(content='', components=[])
+    #     except disnake.errors.HTTPException:
+    #         pass
 
-bot.load_extension("cogs.mycog")
+
+@bot.listen()
+async def on_ready():
+    print("Hatz cu buna dimineata, a pornit botu")
+    
 print("LOADED ZA COG, STARTING ZA BOT")
 # TODO Fac ceva event on_ready() sa anunte ca so logat botu
 bot.run(BOT_TOKEN)
+    
