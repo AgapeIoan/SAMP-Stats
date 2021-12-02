8bc70329f06874b04d52f47d357dad2417f23677
AgapeIoan <44036462+AgapeIoan@users.noreply.github.com>
2021-12-02 20:57:27 +0200

    Inceputa comanda /raportu, actualizat requirements
    Mai trebui lucrat la error handling si rezolvata o interfata mai user friendly

---
+++ functii/samp_server_stats.py
@@ -0,0 +1,30 @@
+from samp_client.client import SampClient
+
+def get_server_data(server_address):
+    try:
+        with SampClient(address=server_address, port=7777) as client:
+            server_data = client.get_server_info()
+        
+        return server_data
+    except Exception as e:
+        print(e)
+        return None
+
+def format_server_data(server_data):
+    # server_data = ServerInfo(password=False, players=219, max_players=1000, hostname='ruby.nephrite.ro - rpg server', gamemode='nephrite, 26 Nov 2021 23:00:21', language='RO/EN')
+    if not server_data:
+        return None, None
+
+    hostname = server_data.hostname
+
+    string_to_return = ""
+    string_to_return += "Gamemode: " + server_data.gamemode + "\n"
+    string_to_return += "Language: " + server_data.language + "\n"
+    string_to_return += "Players: " + str(server_data.players) + "/" + str(server_data.max_players) + "\n"
+        
+    return hostname, string_to_return
+
+if __name__ == '__main__':
+    # Debug
+    x, y = format_server_data(get_server_data(input("Server address: ")))
+    print(x, y)
