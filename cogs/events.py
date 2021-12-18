47ba9d4556f3262022aa67d2723a54922c553480
AgapeIoan <ioanagape14@gmail.com>
2021-12-18 17:18:51 +0200

    Command output + logging

--- cogs/events.py
+++ cogs/events.py
@@ -1,7 +1,12 @@
 # TODO Event on_command for command usage print in console
 # TODO Event on_command_error for command error print in console
 
+import datetime
 import disnake
+import os
+import json
+from disnake import guild
+
 from disnake.ext import commands
 
 from functii.debug import print_debug
@@ -15,29 +20,42 @@ class Events(commands.Cog):
     async def on_slash_command(self, inter):
         # https://prnt.sc/22ksw6z
 
-        print_debug("author: " + str(inter.author))
-        print_debug("channel: " + str(inter.channel))
-        print_debug("channel_id: " + str(inter.channel_id))
-        print_debug("data: " + str(inter.data))
-        print_debug("filled_options: " + str(inter.filled_options))
-        print_debug("guild: " + str(inter.guild))
-        print_debug("guild_id: " + str(inter.guild_id))
-        print_debug("options: " + str(inter.options))
-        print_debug("permissions: " + str(inter.permissions))
-        print_debug("response: " + str(inter.response))
+        # print_debug("author: " + str(inter.author))
+        # print_debug("channel: " + str(inter.channel))
+        # print_debug("channel_id: " + str(inter.channel_id))
+        # print_debug("data: " + str(inter.data))
+        # print_debug("filled_options: " + str(inter.filled_options))
+        # print_debug("guild: " + str(inter.guild))
+        # print_debug("guild_id: " + str(inter.guild_id))
+        # print_debug("options: " + str(inter.options))
+        # print_debug("permissions: " + str(inter.permissions))
 
         data = {
-            "author": inter.author,
-            "channel": inter.channel,
-            "channel_id": inter.channel_id,
-            "data": inter.data,
+            "author": str(inter.author),
+            "author_id": int(inter.author.id),
+            "channel": str(inter.channel),
+            "channel_id": int(inter.channel_id),
             "filled_options": inter.filled_options,
-            "guild": inter.guild,
-            "guild_id": inter.guild_id,
+            "guild": str(inter.guild),
+            "guild_id": int(inter.guild_id),
             "options": inter.options,
-            "permissions": inter.permissions,
-            "response": inter.response
+            "permissions": int(inter.permissions.value),
         }
+
+        # someone used a command
+        print_debug(f"{inter.author} | Guild: {str(inter.guild)} | Channel: {str(inter.channel)} | /{str(inter.data.name)} {str(inter.options)}")
+
+        # log data to unique file
+        za_time = str(datetime.datetime.now()).replace(":", "-")
+        path = r"storage/logs/on_slash_command/" + str(inter.guild_id)
+        log_file_path = path + "/" + f"[{za_time}]" + " " + str(inter.author.id) + ".json"
+        # if the directory doesn't exist, create it
+        if not os.path.exists(path):
+            os.makedirs(path)
+        # dump data to json file
+        with open(log_file_path, "w") as f:
+            json.dump(data, f, indent=4)
+            
         
 
 def setup(bot):
