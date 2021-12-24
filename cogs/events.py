93c3524c7906e28466f07d0d09b4aec37d3736c5
AgapeIoan <ioanagape14@gmail.com>
2021-12-24 02:16:46 +0200

    Logging errors

--- cogs/events.py
+++ cogs/events.py
@@ -15,6 +15,37 @@ class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_slash_command_error(self, inter, error):
+        print_log(f'{error}')
+        # log the error to unique file
+        with open(r'storage/logs/on_slash_command_error/errors.log', 'a') as f:
+            f.write(f'{datetime.datetime.now()} - {error}\n')
+    # LOG THE INTER
+        data = {
+            "author": str(inter.author),
+            "author_id": int(inter.author.id),
+            "channel": str(inter.channel),
+            "channel_id": int(inter.channel_id),
+            "filled_options": inter.filled_options,
+            "guild": str(inter.guild),
+            "guild_id": int(inter.guild_id),
+            "options": inter.options,
+            "permissions": int(inter.permissions.value),
+        }
+
+        # log data to unique file
+        za_time = str(datetime.datetime.now()).replace(":", "-")
+        path = r"storage/logs/on_slash_command_error/" + str(inter.guild_id)
+        log_file_path = path + "/" + f"[{za_time}]" + " " + str(inter.author.id) + ".json"
+        # if the directory doesn't exist, create it
+        if not os.path.exists(path):
+            os.makedirs(path)
+        # dump data to json file
+        with open(log_file_path, "w") as f:
+            json.dump(data, f, indent=4)
+
+
     # Listen for commands
     @commands.Cog.listener()
     async def on_slash_command(self, inter):
