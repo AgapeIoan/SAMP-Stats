463b5b9d4a0251e9a69684bea536d7e894a0d97e
AgapeIoan <ioanagape14@gmail.com>
2022-02-18 11:41:52 +0200

    nush

---
+++ custom_errors.py
@@ -0,0 +1,8 @@
+import disnake
+from disnake.ext import commands
+
+class CommandErrorReport(commands.CommandError):
+    def __init__(self, string_to_send, *args, **kwargs):
+        self.string_to_send = string_to_send
+        super().__init__(*args, **kwargs)
+        
