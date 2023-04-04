76903724380fcff93a8c7c6610b3b31a3f6b2cc0
Agape Ioan <ioanagape14@gmail.com>
2023-04-05 02:31:26 +0300

    Update events.py
    Adus la zi

--- cogs/events.py
+++ cogs/events.py
@@ -16,9 +16,7 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_slash_command_error(self, inter, error):
-        if isinstance(error, custom_errors.CommandErrorReport):
-            print_debug(f"CommandErrorReport: {error.string_to_send}")
-        print_log(f'za error {error}')
+        print_log(f'Error: {error}')
         # log the error to unique file
         with open(r'storage/logs/on_slash_command_error/errors.log', 'a') as f:
             f.write(f'{str(datetime.datetime.now())} - {str(error)}\n')
@@ -39,7 +37,6 @@ class Events(commands.Cog):
             data["channel_id"] = None
             data["guild_id"] = None
 
-
         # log data to unique file
         za_time = str(datetime.datetime.now()).replace(":", "-")
         path = r"storage/logs/on_slash_command_error/" + str(inter.guild_id)
@@ -50,54 +47,6 @@ class Events(commands.Cog):
         # dump data to json file
         with open(log_file_path, "w") as f:
             json.dump(data, f, indent=4)
-
-
-    # Listen for commands
-    @commands.Cog.listener()
-    async def on_slash_command(self, inter):
-        # https://prnt.sc/22ksw6z
-
-        # dir(inter)
-        # for i in dir(inter):
-        #     if i.startswith("_"):
-        #         continue
-        #     print(f"{i}: {getattr(inter, i)}")
-        data = {
-            "author": str(inter.author),
-            "author_id": int(inter.author.id),
-            "filled_options": inter.filled_options,
-            "options": inter.options,
-            "permissions": int(inter.permissions.value),
-            "channel": str(inter.channel),
-            "guild": str(inter.guild),
-        }
-        try:
-            data["channel_id"] = int(inter.channel_id)
-            data["guild_id"] = int(inter.guild_id)
-        except:
-            data["channel_id"] = None
-            data["guild_id"] = None
-
-
-        # someone used a command
-        print_log(f"{inter.author} | Guild: {str(inter.guild)} | Channel: {str(inter.channel)} | /{str(inter.data.name)} {str(inter.options)}")
-
-        # log data to unique file
-        za_time = str(datetime.datetime.now()).replace(":", "-")
-        path = r"storage/logs/on_slash_command/" + str(inter.guild_id)
-        log_file_path = path + "/" + f"[{za_time}]" + " " + str(inter.author.id) + ".json"
-        # if the directory doesn't exist, create it
-        if not os.path.exists(path):
-            os.makedirs(path)
-        # dump data to json file
-        with open(log_file_path, "w") as f:
-            json.dump(data, f, indent=4)
-
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        # This works only on legacy commands, the ones using @bot.command() decorator
-        print_debug(f"on_command_error: {error}")
-
 
 def setup(bot):
     bot.add_cog(Events(bot))
